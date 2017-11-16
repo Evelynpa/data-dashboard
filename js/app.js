b29b92f2e2dade4412651fbe8886400862be9d57
Evelynpa <evelyn.parra.rojas@gmail.com>
2017-11-16 18:38:49 -0300

    se realiza menu y ejemplos de graficos

--- js/app.js
+++ js/app.js
@@ -3,4 +3,78 @@
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+
+ // Load the Visualization API and the corechart package.
+      google.charts.load('current', {'packages':['corechart']});
+// Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(dibujar);
+      //google.setOnLoadCallback(dibujar);
+
+function dibujar() {
+// Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Comidas');
+        data.addColumn('number', 'Visitas');
+        data.addRows([
+          ['Mushrooms', 3],
+          ['Onions', 134],
+          ['Olives', 14],
+          ['Zucchini', 19],
+          ['Pepperoni', 29]
+        ]);
+
+        // Set chart options
+        var options = {'title':'Visitas de Comidas',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      };
+
+      console.log(data);
+
+      /******************************************************************************************/
+google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawBasic);
+
+function drawBasic() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('timeofday', 'Time of Day');
+      data.addColumn('number', 'Motivation Level');
+
+      data.addRows([
+        [{v: [8, 0, 0], f: '8 am'}, 1],
+        [{v: [9, 0, 0], f: '9 am'}, 2],
+        [{v: [10, 0, 0], f:'10 am'}, 3],
+        [{v: [11, 0, 0], f: '11 am'}, 4],
+        [{v: [12, 0, 0], f: '12 pm'}, 5],
+        [{v: [13, 0, 0], f: '1 pm'}, 6],
+        [{v: [14, 0, 0], f: '2 pm'}, 7],
+        [{v: [15, 0, 0], f: '3 pm'}, 8],
+        [{v: [16, 0, 0], f: '4 pm'}, 9],
+        [{v: [17, 0, 0], f: '5 pm'}, 10],
+      ]);
+
+      var options = {
+        title: 'Motivation Level Throughout the Day',
+        hAxis: {
+          title: 'Time of Day',
+          format: 'h:mm a',
+          viewWindow: {
+            min: [7, 30, 0],
+            max: [17, 30, 0]
+          }
+        },
+        vAxis: {
+          title: 'Rating (scale of 1-10)'
+        }
+      };
+
+      var chart = new google.visualization.ColumnChart(
+        document.getElementById('chart_div2'));
+
+      chart.draw(data, options);
+    }
